ba88cea2e24edc7741a56aa64164fd7cc3e9c059
Muhammad Aiman <m.aimanisa@live.iium.edu.my>
2024-01-17 19:12:46 +0800

    #final

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:ramadhan_app/splash.dart';
 
-void main() {
+Future<void> main() async {
+  //init the hive
+  await Hive.initFlutter();
+
+  // open a box
+  var box = await Hive.openBox('mybox');
   runApp(const MainApp());
 }
 
